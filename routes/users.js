e520fd8398f63659a95294c5a6c07421034d89ce
Patrick Pruß <pruss.patrick@googlemail.com>
2019-06-18 20:00:16 +0200

    Login token and token based auth

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const {check, validationResult} = require('express-validator/check');
 
 const User = require('../models/User');
@@ -21,22 +23,35 @@ router.post('/', [
     const { name, email, password} = req.body;
 
     try{
+        // User already exists?
         let user = await User.findOne({email});
 
         if(user){
             return res.status(400).json({msg: 'User already exists'});
         }
 
+        // Create user and hash password
         user = new User({name, email, password});
-
         const salt = await bcrypt.genSalt(10);
         user.password = await bcrypt.hash(password, salt);
         await user.save();
+        
+        // Token creation
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
 
-        res.send('User saved');
+        jwt.sign(payload, config.get('jwtSecret'), {
+            expiresIn: 3600
+        }, (err, token) => {
+            if(err) throw err;
+            res.json({token});
+        });
     } catch(err) {
         console.error(err.message);
-        res.send('404')
+        res.status(500).send('Server Error');
     }
 });
 
